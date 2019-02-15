35ccbfab2c14b83877d4b976a6fcb7c50d17eb7f
Nishant Shah <nishantshahuci@gmail.com>
2019-02-15 15:19:33 -0500

    Initial file setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+// import React
+import React from 'react';
+
+// import components
+import SearchBar from './SearchBar';
+
+// App component
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+// export component
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+// import React
+import React, { Component } from 'react';
+
+// SearchBar component
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
+        );
+    }
+}
+
+// export component
+export default SearchBar;
